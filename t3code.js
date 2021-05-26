365ef0a4468438de4758fbe02c79c92db8871c38
ali-create <57019728+ali-create@users.noreply.github.com>
2021-05-26 00:42:33 -0400

    Update t3code.js

--- t3code.js
+++ t3code.js
@@ -1,17 +1,19 @@
 let word = prompt("Sentence?");
-let chance = Math.trunc(Math.random() * prompt("Chance? (only an number)"));
+let promptAnswer = prompt("Chance? (only an number)");
+let chance = Math.trunc(Math.random() * promptAnswer);
 let output;
 let letter;
 let upperCaseLetter = "";
 let numberOfLetter = 1;
 for (let i = 0; i < word.length + 1; i++) {
+  console.log(chance);
   letter = word.charAt(numberOfLetter);
-  chance = Math.trunc(Math.random() * 2);
+  chance = Math.trunc(Math.random() * promptAnswer);
   numberOfLetter++;
   if (chance == 1) {
     upperCaseLetter = letter.toUpperCase();
     word = word.replace(letter, upperCaseLetter);
-    console.log([i]);
+    letter = word.charAt(numberOfLetter);
   }
 }
 alert(word);
